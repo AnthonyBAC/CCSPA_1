76b150e85f1e483ffd2cf128f8fdc1209e8a76b9
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-02-13 15:23:13 -0300

    change colors redes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
@@ -9,6 +10,7 @@ export default defineConfig({
     },
     devToolbar:{
         enabled: false
-    }
+    },
+    
     
 });
