1c85699d1363ee5dde219c4c40c6b72fee2793e3
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-02-13 13:25:18 -0300

    Creditos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
+    },
+    devToolbar:{
+        enabled: false
     }
+    
 });
