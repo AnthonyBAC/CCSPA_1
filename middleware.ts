a828e6528a11377338a87f5c6c6e3e9f07efa4b8
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-02-13 15:43:42 -0300

    middl2

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  const allowedIP = "XXX.XXX.XXX.XXX"; // Cambia esto por tu IP pública.
+  const allowedIP = "190.47.102.181"; // Cambia esto por tu IP pública.
   const clientIP = context.request.headers.get("x-forwarded-for");
 
   if (clientIP !== allowedIP) {
