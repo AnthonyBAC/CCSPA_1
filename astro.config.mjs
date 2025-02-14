8bec0e32e0abcff25db9ec577e8c129934d514c2
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-02-14 00:16:06 -0300

    form finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    devToolbar:{
-        enabled: false
-    },
+  vite: {
+      plugins: [tailwindcss()]
+  },
 
-    
-    
+  devToolbar:{
+      enabled: false
+  },
+  
+
+  adapter: vercel(),
+  output: 'server',
+
+
+
 });
